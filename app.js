07b26dd29618d96a0b1b0639dbb4bb7938182001
And1210 <amf7crazy@gmail.com>
2017-11-08 23:33:22 -0500

    Added Russ's html as the serving webpage

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const server = express();
 const path = require('path');
 
 server.use(express.static('public'));
+server.use(express.static('HTML'));
 
 server.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/HTML/listskeleton.html'));
 });
 
 server.listen(8000, function () {
