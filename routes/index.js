0ac5549af85b5920fe6258e7223e88bb3fee7d30
ruffoa <ruffo.alex@gmail.com>
2017-11-09 15:28:51 -0500

    Added is room currently booked to the page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var date = '11-29-2017';
+var date = '10-23-2017';
 var room = 3;
 var bookingCount = 'test';
 var jsonObj;
+var isFreeNow;
 
 /* GET home page. */
 function getDate(){
@@ -38,21 +39,41 @@ http.get(url, function(res){
 });
 
 function checkRoomAvaliable(json){
+    var dateObj = new Date();
+    var current_hour = dateObj.getHours();
+    var isFree = true;
 
-    var count = 0;
+
     for (var booking in json){
         console.log("Booking Start: " + booking +", value: " + JSON.stringify(json[booking]));
-        count ++;
+        var roomStr = JSON.stringify(json[booking]);
+        console.log("RoomString:\n" + roomStr);
+        var start = roomStr.substr(roomStr.indexOf('StartTime') + 12);
+        start = start.substr(start.indexOf('T') +1);
+        start = start.substr(0, start.indexOf(':'));
+        var end = roomStr.substr(roomStr.indexOf('EndTime') + 6);
+        end = end.substr(end.indexOf('T') +1);
+        end = end.substr(0, end.indexOf(':'));
+
+        if((start <= current_hour && end > current_hour)){
+            console.log('Found a time!');
+            isFree = false;
+            break;
+        }
+
+        console.log("\nStartTime " + start + " EndTime " + end + " CurrentHour " + current_hour );
     }
+
     jsonObj = json;
+    bookingCount = Object.keys(json).length;
     // bookingCount = Object.keys(JSON).length;
-    bookingCount = count;
-    console.log("COUNT: ", bookingCount);
+    console.log("COUNT: ", bookingCount + " Is the room free: " + isFree);
+    isFreeNow = isFree;
 }
 
 router.get('/', function(req, res, next) {
     var string = 'https://queensu.evanced.info/dibs/Login';
-    res.render('index', { title: 'Dibs Wrapper Test', srcStr: string, count: bookingCount, jaderoom: room, jadedate: date, jadeJson: jsonObj});
+    res.render('index', { title: 'Dibs Wrapper Test', srcStr: string, count: bookingCount, jaderoom: room, jadedate: date, jadeJson: jsonObj, jadeRoomFreeNow: isFreeNow});
 
 });
 
