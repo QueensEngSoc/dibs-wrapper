42f3fb78ea27fcc3dd369f405372fe7a49820aa1
Michael Albinson <mikealbinson@gmail.com>
2017-11-16 00:36:33 -0500

    Merge to maintain my changes -> cleanup of booking
    And notes on potential flaws

--- book_room.js
+++ book_room.js
@@ -1,72 +1,45 @@
 const $ = require('najax');
 
-function checkReservation(f, l, e) {
+function checkReservation(firstName, lastName, phoneNumber, emailAddress, roomID, dateTime, resLength) {
     var dibsWSURL = 'https://queensu.evanced.info/admin/dibs/api/reservations/post';
     var reservationSent = false;
     if (!reservationSent) {
         reservationSent = true;
-        var firstName, lastName, phoneNumber, emailAddress;
-
-        // if ($.trim($('#FirstName').val()) != '*First Name') {
-        //     firstName = $.trim($('#FirstName').val());
-        // } else {
-        //     firstName = "";
-        // }
-        //
-        // if ($.trim($('#LastName').val()) != '*Last Name') {
-        //     lastName = $.trim($('#LastName').val());
-        // } else {
-        //     lastName = "";
-        // }
-        //
-        // if ($.trim($('#Phone').val()) != 'Phone# (for text confirmation)') {
-        //     phoneNumber = $.trim($('#Phone').val());
-        // } else {
-        //     phoneNumber = "";
-        // }
-        //
-        //
-        // if ($.trim($('#EmailAddress').val()) != '*Email') {
-        //     emailAddress = $.trim($('#EmailAddress').val());
-        // } else {
-        //     emailAddress = "";
-        // }
-
-        firstName = f;
-        lastName = l;
-        emailAddress = e;
-        phoneNumber = "";
 
         var postData = {
             firstName: firstName,
             lastName: lastName,
-            roomid: "21",//$('#SelectedRoomID').val(),
-            startDate: "2017/11/21 10:30:00",//$('#SelectedStartTime').val(),
-            reservationLength: "1",//$('#SelectedTime').val(),
+            roomid: roomID,
+            startDate: dateTime,
+            reservationLength: resLength,
             phoneNumber: phoneNumber,
             emailAddress: emailAddress,
-            langCode: "en-US",//$('#SelectedLang').val(),
+            langCode: "en-US",
             staffAccess: false
         };
 
-        $({
-            type: "POST",
+        $.post({
             url: dibsWSURL,
             data: JSON.stringify(postData),
             contentType: "application/json; charset=utf-8",
             async: true,
             success: function(objReturn) {
-                console.log(objReturn);
-                if (objReturn.IsSuccess == true) {
+                if (objReturn.IsSuccess === true) {
                     console.log('Submitted!');
-                    verifySubmitBool = true;
                 } else {
-                    reservationSent = false;
+                    console.log('Booking Success: ' + objReturn);
                 }
             },
             error: function(xmlHttpRequest) {
+                console.log("SEVERE ERROR trying to contact the dibs server");
             }
         });
     }
 }
-checkReservation('Andrew', 'Farley', 'amf7crazy@gmail.com');
+checkReservation("Michael", "Albinson", "613 305 0359", "13ma100@queensu.ca", "24", "2017/11/22 22:30:00", "1");
+// can book without a password (skips the entire verification process)
+// can book without a queens email
+// can book for up to 14 hours a day
+// can book for any time in perpetuity beyond the current date
+// Only restriction is that you can not have 2 reservations for the same room at any point in time AND
+// you can only have a maximum of 4 reservations at a time
