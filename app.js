517072ede2e8c7e4d9cab4521bdde2567a41bd73
And1210 <amf7crazy@gmail.com>
2017-11-22 21:35:54 -0500

    Starting work of sql

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+const Sequelize = require('sequelize');
 const express = require('express');
-const server = express();
 const path = require('path');
+
+const server = express();
+const sequelize = new Sequelize('database', 'username', 'password',
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+    });
+//console.log(sequelize);
 
 server.use('/public', express.static('public'));
 server.use('/HTML', express.static('HTML'));
