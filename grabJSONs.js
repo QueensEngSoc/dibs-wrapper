3bd5dcd544ba5d5c6e70c49ea52be4948cf663ef
And1210 <amf7crazy@gmail.com>
2017-12-30 15:01:47 -0500

    Grab JSONs modified
    If you have a mongodb database you can run grabJSONs and it will put all
    the dibs room jsons into the collection roomInfo

--- grabJSONs.js
+++ grabJSONs.js
@@ -2,14 +2,14 @@
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/roomDatabase');
-var data = db.get('data');
+var roomInfo = db.get('roomInfo');
 
 var request = require('request');
 var str = "https://queensu.evanced.info/dibsAPI/rooms";
 
 request(str, function (err, res, body) {
     for (var json in JSON.parse(body)) {
-        data.insert(JSON.parse(body)[json]);
+        roomInfo.insert(JSON.parse(body)[json]);
         console.log(JSON.parse(body)[json]);
     }
 });
