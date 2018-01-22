089ebac645af49b74c17e77c566c814472575446
ruffoa <ruffo.alex@gmail.com>
2018-01-21 19:06:34 -0500

    Fixed a bug with regards to start times, and me being kinda dumb.

--- routes/map.js
+++ routes/map.js
@@ -11,7 +11,12 @@ router.get('/map', function (req, res, next) { //the request to render the page
 
     var dateObj = new Date();
     var current_hour = dateObj.getHours();
+    var current_min = dateObj.getMinutes();
 
+    if (current_min < 30)   // logic here is that we are returning the status based on the start hour.  Since the min booking time is
+        current_hour --;    // 1 hour, if the current minute is less than 30, we are still within the previous booking slot
+                            // and we should therefore subtract 1 from the hour to get the right data (eg. if it is 7:10pm
+                            // right now, then we really want the data from 6:30 - 7:30, not 7:30 - 8:30)
 
     roomFuncs.getListOfRoomState(dateObj.getDate(), current_hour).then(function(listFree){
         var testStatus = [];
