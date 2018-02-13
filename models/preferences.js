d106005e32a482d1e0fd36d628129c82f9da14ce
ruffoa <ruffo.alex@gmail.com>
2018-02-12 23:15:07 -0500

    Added the preferences backend file for Michael.  It is preferences.js in models

--- models/preferences.js
+++ models/preferences.js
@@ -1,6 +1,40 @@
 var consts = require('../config/config');
 var User = require('../models/user');
+var userFuncs = require('userFunctions');
 
-function getPrefsObj(){
+function getPrefsObj(req) {
+    return new Promise(function (resolve, reject) {
+        if (req.isAuthenticated()) {
+            var user = req.user;
+            var json = JSON.parse(user.local.preferences);
 
+            resolve(json);
+        }
+        else{
+            resolve({});
+        }
+    });
+}
+
+/**
+ * updatePrefsObj returns true if it updated successfully, and false if not
+ * @param req, data  -> to get the user.  Data should be a JSON object
+ * @returns {true or false}
+ */
+function updatePrefsObj(req, data){
+    return new Promise(function (resolve, reject) {
+        if (req.isAuthenticated()) {
+            var user = req.user;
+            var jsonStr = JSON.stringify(data);
+            user.local.preferences = jsonStr;
+
+            User.findOneAndUpdate({'local.email': user.local.email}, {'local.preferences': user.local.preferences}, function (err, resp) {
+                console.log("Updated user preferences!");
+                resolve(true);
+            });
+        }
+        else{
+            resolve(false);
+        }
+    });
 }
