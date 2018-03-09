cb09eb1c4ab738383727e757222c7aa5c34a5ecd
ruffoa <ruffo.alex@gmail.com>
2018-03-09 11:28:28 -0500

    Hopefully fixed issue where heroku app was being spun down every hour, breaking the dayShifting code if it was in sleep mode

--- app.js
+++ app.js
@@ -138,6 +138,15 @@ server.listen(port, function () {
     console.log('Example app listening on port ' + port + "!");
 });
 
+if (env != 'dev'){
+    var http = require("http");
+    console.log("Setting up ping service to keep the Heroku site awake (fixing the lag and dayShifting code breaking)");
+    setInterval(function() {
+        http.get("https://dibs-beta.herokuapp.com.herokuapp.com");
+        console.log("Ping......");
+    }, 300000); // every 5 minutes (300000)
+}
+
 dbFuncs.setupEndOfDayScript();
 module.exports = server;
 
